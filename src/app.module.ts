4fdcbc7305a6a55b29d0cf19f3953390d0c5320e
JM REMEUR <jm@remeur.fr>
2021-01-04 00:55:32 +0100

    using one module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
-import { AuthModule } from './controllers/auth/auth.module';
-import { IdModule } from './controllers/id/id.module';
+import { R6Service } from './r6.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './controllers/auth/auth.service';
+import { JwtStrategy } from './controllers/auth/jwt.strategy';
+import { IdController } from './controllers/id/id.controller';
 
 @Module({
   imports: [
-    AuthModule,
-    IdModule,
+    JwtModule.register({ secret: process.env.SECRET }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [
+    AuthController,
+    IdController
+  ],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    R6Service
+  ],
 })
 export class AppModule { }
