8fc5c733c095b8381f6b832a08cee1bc3047f866
JM REMEUR <jm@remeur.fr>
2021-01-04 12:40:42 +0100

    refactor tasks handle enabling and disabling cron job

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,32 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 
 import { R6Service } from './r6.service';
 import { DatabaseService } from './database.service';
+import { CronJob } from 'cron';
 
 @Injectable()
 export class TasksService {
 
   players: Array<string>;
-  platform: string;
+  platform = process.env.PLATFORM || 'uplay';
+  cron = process.env.CRON || CronExpression.EVERY_MINUTE;
+  cronJobName = 'stats';
+  cronJob: CronJob;
 
   constructor(
     private readonly r6Service: R6Service,
-    private readonly databaseService: DatabaseService
+    private readonly databaseService: DatabaseService,
+    private readonly schedulerRegistry: SchedulerRegistry
   ) {
     const players = process.env.PLAYERS || '';
     this.players = players.split(',');
-    this.platform = process.env.PLATFORM || 'uplay';
 
-  }
-
-  @Cron(process.env.CRON || CronExpression.EVERY_MINUTE)
-  saveStats() {
-    this.players.forEach(async (playerName) => {
-      const stats = await this.r6Service.getStatsByUsername(this.platform, playerName);
-
-      stats.player = playerName;
-      await this.databaseService.saveStats(stats);
+    this.cronJob = new CronJob(this.cron, () => {
+      this.players.forEach(async (playerName) => {
+        const stats = await this.r6Service.getStatsByUsername(this.platform, playerName);
+  
+        stats.player = playerName;
+        await this.databaseService.saveStats(stats);
+      });
     });
+
+    if(process.env.ENABLE_STATS_UPDATE || false) {
+      this.schedulerRegistry.addCronJob(this.cronJobName, this.cronJob);
+    }
   }
 }
