7d6b53219e51c2722df3782a958eb61e35fc563a
JM REMEUR <jm@remeur.fr>
2021-01-04 16:05:58 +0100

    revert to normal

--- src/services/r6.service.ts
+++ src/services/r6.service.ts
@@ -4,22 +4,14 @@ const R6API = require('r6api.js');
 @Injectable()
 export class R6Service {
 
-  r6Api: any;
-
-  getR6Api(): any {
-    if(this.r6Api == null) {
-      this.r6Api = new R6API(process.env.EMAIl, process.env.PASSWORD);
-    }
-
-    return this.r6Api;
-  }
+  r6Api = new R6API(process.env.EMAIl, process.env.PASSWORD);
 
   async getId(platform: string, username: string): Promise<string> {
-    return await this.getR6Api().getId(platform, username).then(el => el[0].id);
+    return await this.r6Api.getId(platform, username).then(el => el[0].id);
   }
 
   async getStats(platform: string, id: string) {
-    return await this.getR6Api().getStats(platform, id).then(el => el[0]);
+    return await this.r6Api.getStats(platform, id).then(el => el[0]);
   }
 
   async getStatsByUsername(platform: string, username: string) {
