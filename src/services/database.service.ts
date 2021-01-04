7726f2593239297ff8a4336159963d69e86ce457
JM REMEUR <jm@remeur.fr>
2021-01-04 02:31:12 +0100

    if the last data is the same we only update the timestamp

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Database } from 'arangojs';
+import { aql, Database } from 'arangojs';
 import { DocumentCollection } from 'arangojs/collection';
 
 @Injectable()
@@ -23,11 +23,42 @@ export class DatabaseService {
     const exist = await this.collection.exists();
     if(!exist) {
       await this.collection.create();
+      await this.collection.ensureIndex({
+        type: 'persistent',
+        fields: ['player']
+      });
     }
   }
 
+  async getLastPlayerStats(player: string) {
+    const query = await this.db.query(aql`
+      FOR doc in ${this.collection}
+      FILTER doc.player == ${player}
+      SORT doc.timestamp DESC
+      LIMIT 1
+      return doc
+    `);
+
+    const data = await query.all();
+    return data.length > 0 ? data[0] : null;
+  }
+
   async saveStats(stats: any) {
-    stats.timestamp = new Date().getTime();
-    await this.collection.save(stats);
+
+    const lastPlayerStats = await this.getLastPlayerStats(stats.player);
+
+    const pve = JSON.stringify(stats.pve) == JSON.stringify(lastPlayerStats?.pve);
+    const pvp = JSON.stringify(stats.pvp) == JSON.stringify(lastPlayerStats?.pvp);
+
+    const timestamp = new Date().getTime();
+
+    if(pve && pvp) {
+      await this.collection.update(lastPlayerStats, {
+        timestamp
+      });
+    }
+    else {
+      await this.collection.save(stats);
+    }
   }
 }
