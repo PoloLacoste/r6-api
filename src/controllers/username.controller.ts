250fd8406678a4039d9a50b944656db596e7060a
JM REMEUR <jm@remeur.fr>
2021-01-17 00:38:38 +0100

    username swagger documentation

--- src/controllers/username.controller.ts
+++ src/controllers/username.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { R6Service } from 'src/services/r6.service';
 import { PlayerUsername } from 'src/models/player-username';
+import { PlatformId } from 'src/models/platform-id';
+import { PlatformType } from 'src/models/platform-type';
 
 @Controller('username')
 @ApiTags('Username')
@@ -11,10 +13,25 @@ export class UsernameController {
   constructor(private readonly r6Service: R6Service) {}
 
   @Get(':id')
-  async getUsername(
-    @Param('platform') platform: string,
-    @Param('id') id: string
-  ): Promise<PlayerUsername | null> {
-    return await this.r6Service.getUsername(platform, id);
+  @ApiOperation({
+    description: 'Get the username of a player by his id',
+  })
+  @ApiParam({
+    name: 'platform',
+    description: 'Platform of the player',
+    enum: PlatformType,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Id of the player',
+    type: String
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Username of a player',
+    type: PlayerUsername
+  })
+  async getUsername(@Param() params: PlatformId): Promise<PlayerUsername | null> {
+    return await this.r6Service.getUsername(params.platform, params.id);
   }
 }
