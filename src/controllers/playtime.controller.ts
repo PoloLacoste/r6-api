62f743068582dcecc1b31841cc7a475723987258
JM REMEUR <jm@remeur.fr>
2021-01-17 00:01:56 +0100

    playtime documentation

--- src/controllers/playtime.controller.ts
+++ src/controllers/playtime.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { R6Service } from 'src/services/r6.service';
 import { PlayerPlaytime } from 'src/models/player-playtime';
+import { PlatformType } from 'src/models/platform-type';
+import { PlatformId } from 'src/models/platform-id';
+import { PlatformUsername } from 'src/models/platform-username';
 
 @Controller('playtime')
 @ApiTags('Playtime')
@@ -11,18 +14,48 @@ export class PlaytimeController {
   constructor(private readonly r6Service: R6Service) {}
 
   @Get('id/:id')
-  async getPlaytimeById(
-    @Param('platform') platform: string,
-    @Param('id') id: string
-  ): Promise<PlayerPlaytime | null> {
-    return await this.r6Service.getPlaytimeById(platform, id);
+  @ApiOperation({
+    description: 'Get the playtime of a player by his id',
+  })
+  @ApiParam({
+    name: 'platform',
+    description: 'Platform of the player',
+    enum: PlatformType,
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Id of the player',
+    type: String
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Playtime of a player',
+    type: PlayerPlaytime
+  })
+  async getPlaytimeById(@Param() params: PlatformId): Promise<PlayerPlaytime | null> {
+    return await this.r6Service.getPlaytimeById(params.platform, params.id);
   }
 
   @Get('username/:username')
-  async getPlaytimeByUsername(
-    @Param('platform') platform: string,
-    @Param('username') username: string
-  ): Promise<PlayerPlaytime | null> {
-    return await this.r6Service.getPlaytimeByUsername(platform, username);
+  @ApiOperation({
+    description: 'Get the playtime of a player by his username',
+  })
+  @ApiParam({
+    name: 'platform',
+    description: 'Platform of the player',
+    enum: PlatformType,
+  })
+  @ApiParam({
+    name: 'username',
+    description: 'Username of the player',
+    type: String
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Playtime of a player',
+    type: PlayerPlaytime
+  })
+  async getPlaytimeByUsername(@Param() params: PlatformUsername): Promise<PlayerPlaytime | null> {
+    return await this.r6Service.getPlaytimeByUsername(params.platform, params.username);
   }
 }
--- src/models/player-playtime.ts
+++ src/models/player-playtime.ts
@@ -1,7 +1,19 @@
-export interface PlayerPlaytime {
+import { ApiProperty } from "@nestjs/swagger";
+
+export class PlayerPlaytime {
+    
+    @ApiProperty()
     id:        string;
+    
+    @ApiProperty()
     general:   number;
+    
+    @ApiProperty()
     ranked:    number;
+    
+    @ApiProperty()
     casual:    number;
+    
+    @ApiProperty()
     discovery: number;
 }
