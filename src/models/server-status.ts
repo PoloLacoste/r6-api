b1df884dba47a3bb6631afa5d3ced3a039f31b73
JM REMEUR <jm@remeur.fr>
2021-01-16 23:46:17 +0100

    swagger documentation for servers status

--- src/models/server-status.ts
+++ src/models/server-status.ts
@@ -1,17 +1,41 @@
-export interface ServerStatus {
-    PC:   PC;
-    PS4:  PC;
-    XBOX: PC;
+import { ApiProperty } from "@nestjs/swagger";
+
+export class PC {
+  @ApiProperty()
+  "AppID ": string;
+
+  @ApiProperty()
+  MDM: string;
+
+  @ApiProperty()
+  SpaceID: string;
+
+  @ApiProperty()
+  Category: string;
+
+  @ApiProperty()
+  Name: string;
+
+  @ApiProperty()
+  Platform: string;
+
+  @ApiProperty()
+  Status: string;
+
+  @ApiProperty()
+  Maintenance: string | null;
+
+  @ApiProperty()
+  ImpactedFeatures: any[];
 }
 
-export interface PC {
-    "AppID ":         string;
-    MDM:              string;
-    SpaceID:          string;
-    Category:         string;
-    Name:             string;
-    Platform:         string;
-    Status:           string;
-    Maintenance:      null;
-    ImpactedFeatures: any[];
+export class ServerStatus {
+  @ApiProperty()
+  PC: PC;
+
+  @ApiProperty()
+  PS4: PC;
+
+  @ApiProperty()
+  XBOX: PC;
 }
