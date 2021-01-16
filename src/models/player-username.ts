7ebd9a624502e88d006dfc679274dc2f440a71ad
JM REMEUR <jm@remeur.fr>
2021-01-17 00:38:38 +0100

    username swagger documentation

--- src/models/player-username.ts
+++ src/models/player-username.ts
@@ -1,6 +1,16 @@
-export interface PlayerUsername {
+import { ApiProduces, ApiProperty } from "@nestjs/swagger";
+
+export class PlayerUsername {
+
+    @ApiProperty()
     id:       string;
+    
+    @ApiProperty()
     userid:   string;
+    
+    @ApiProperty()
     username: string;
+    
+    @ApiProperty()
     platform: string;
 }
