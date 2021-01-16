18322ea674b030de1db6ae33c7b0c77fa356995f
JM REMEUR <jm@remeur.fr>
2021-01-16 23:54:19 +0100

    level swagger documentation

--- src/models/player-level.ts
+++ src/models/player-level.ts
@@ -1,11 +1,25 @@
-export interface PlayerLevel {
-    id:                 string;
-    level:              number;
-    xp:                 number;
-    lootboxProbability: LootboxProbability;
+import { ApiProperty } from "@nestjs/swagger";
+
+export class LootboxProbability {
+  
+  @ApiProperty()
+  raw: number;
+  
+  @ApiProperty()
+  percent: string;
 }
 
-export interface LootboxProbability {
-    raw:     number;
-    percent: string;
+export class PlayerLevel {
+  
+  @ApiProperty()
+  id: string;
+  
+  @ApiProperty()
+  level: number;
+  
+  @ApiProperty()
+  xp: number;
+  
+  @ApiProperty()
+  lootboxProbability: LootboxProbability;
 }
