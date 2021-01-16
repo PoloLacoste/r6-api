93eba2471dc6e24b6789109c34f319f66b0d3595
JM REMEUR <jm@remeur.fr>
2021-01-16 23:46:17 +0100

    swagger documentation for servers status

--- src/controllers/status.controller.ts
+++ src/controllers/status.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { R6Service } from 'src/services/r6.service';
 import { ServerStatus } from 'src/models/server-status';
@@ -10,6 +10,14 @@ export class StatusController {
   constructor(private readonly r6Service: R6Service) { }
 
   @Get()
+  @ApiOperation({
+    description: 'Get the status of all servers',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Status of all servers',
+    type: ServerStatus
+  })
   async getServersStatus(): Promise<ServerStatus> {
     return await this.r6Service.getServersStatus();
   }
