a755dd368bc92199505ed51bad58e361d71a968c
JM REMEUR <jm@remeur.fr>
2021-01-16 22:28:02 +0100

    use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 require('dotenv').config()
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
