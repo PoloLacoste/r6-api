7b4e2db201fb2c423272f5c9af77c58dd015485b
JM REMEUR <jm@remeur.fr>
2021-01-20 01:33:22 +0100

    create data for tests

---
+++ test/data.ts
@@ -0,0 +1,21 @@
+import { PlatformType } from "src/models/platform-type";
+
+export const platform = PlatformType.Uplay;
+export const username = 'Godly';
+export const id = 'be3313d6-d443-4eae-818f-bb7f56837781';
+export const level = {
+  "id": "be3313d6-d443-4eae-818f-bb7f56837781",
+  "level": 377,
+  "xp": 137702,
+  "lootboxProbability": {
+    "raw": 4320,
+    "percent": "43.20%"
+  }
+};
+export const playtime = {
+  "id": "be3313d6-d443-4eae-818f-bb7f56837781",
+  "general": 6248491,
+  "ranked": 5608031,
+  "casual": 499425,
+  "discovery": 141035
+};
