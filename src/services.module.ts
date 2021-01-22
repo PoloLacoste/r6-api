abdc886ea4dbde2115df1fe4069e6524de260e9d
JM REMEUR <jm@remeur.fr>
2021-01-22 14:17:25 +0100

    changed service provider updating initialization

--- src/services.module.ts
+++ src/services.module.ts
@@ -2,16 +2,31 @@ import { Module } from '@nestjs/common';
 
 import { CacheService, Database, MongoDatabase, R6Service } from 'r6-api-cacher';
 
+const cacheService = new CacheService(process.env.REDIS_URL);
+const database = new MongoDatabase(process.env.MONGO_URL);
+const r6Service = new R6Service(process.env.EMAIL, process.env.PASSWORD, {
+  caching: Boolean(JSON.parse(process.env.CACHING || "false")),
+  expiration: parseInt(process.env.EXPIRATION || "60000"),
+  cacheService: cacheService,
+  database: database
+});
+
 @Module({
   imports: [
   ],
   providers: [
     {
-      provide: Database,
-      useFactory: () => new MongoDatabase()
+      provide: CacheService,
+      useFactory: () => cacheService
     },
-    R6Service,
-    CacheService
+    {
+      provide: Database,
+      useFactory: () => database
+    },
+    {
+      provide: R6Service,
+      useFactory: () => r6Service
+    }
   ],
   exports: [
     Database,
