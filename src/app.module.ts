05473c28249e7db9d1610ceeb51da86bca6032bb
JM REMEUR <jm@remeur.fr>
2023-09-01 21:59:56 +0200

    bumps deps migrate to pnpm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,19 @@
 import { Module } from '@nestjs/common';
-import { RouterModule, Routes } from 'nest-router';
+import { RouterModule } from '@nestjs/core';
 
 import { PlatformModule } from './platform.module';
 import { ServicesModule } from './services.module';
 
 import { StatusController } from './controllers/status.controller';
 
-const routes: Routes = [
-  {
-    path: '/:platform',
-    module: PlatformModule,
-  }
-];
-
 @Module({
   imports: [
-    RouterModule.forRoutes(routes),
+    RouterModule.register([
+      {
+        path: '/:platform',
+        module: PlatformModule,
+      }
+    ]),
     PlatformModule,
     ServicesModule
   ],
